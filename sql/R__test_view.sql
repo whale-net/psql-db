d4eba495f20a981558145326797f38d77adef47e
qmastermoo <cpsaicontact@gmail.com>
2022-12-04 01:21:37 -0500

    update view to test

--- sql/R__test_view.sql
+++ sql/R__test_view.sql
@@ -1,7 +1,13 @@
+-- because this file is prefixed with R__, it will run whenever it has been modified (chekcsum changes)
+-- this is useful for things like views and functions
+-- this is an example of a view, you can modify this if you want to see how it works
 CREATE OR REPLACE VIEW flyway_test.v_test_data_extra_comments
 AS
     SELECT td.id as test_data_id, tdec.id as test_data_extra_comment_id
         , td.test_val, tdec.comment
+        , tdec.id as dummy_id_val_for_a_test_pleas_ignore
     FROM flyway_test.test_data td
     INNER JOIN flyway_test.test_data_extra_comments tdec on td.id = tdec.test_data_id
     ;
+
+-- although apparently if you want to rename a column, it will have to be altered? I'm not sure how to handle that yet
